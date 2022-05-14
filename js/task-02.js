349bbd086e3f485391e8e17dfb575fe7d801a8d5
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-05-14 16:12:28 +0300

    рабить таск 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const navItemEl = document.querySelector('ul#ingredients')
+console.log(navItemEl)
+  
+  
+ingredients.forEach(element => {
+  let ingredEl = document.createElement("li");
+  ingredEl.textContent = element;
+  ingredEl.classList.add('item');
+  
+  navItemEl.appendChild(ingredEl);
+});
+
+
