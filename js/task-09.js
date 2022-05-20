90dc9cd0cc5bd913cee31e19c89d204ce1bfe50e
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-05-20 11:20:17 +0300

    виконує таск 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const changeColorEl = document.querySelector('button.change-color');
+const spanColorEl = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorEl.addEventListener('click', valueChangeColor);
+
+function valueChangeColor() {
+  console.log('click')
+  spanColorEl.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
