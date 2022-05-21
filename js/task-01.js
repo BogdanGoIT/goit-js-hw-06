0943e32ffb351e9f33d4f951971f26527719ba25
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-05-21 20:13:48 +0300

    коректує таск 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,24 @@
-const categoryEl = document.querySelectorAll('li.item');
+const categoryEl = document.querySelectorAll('ul#categories li.item');
 
 console.log(`Number of categories: ${categoryEl.length}`);
 
 for (const category of categoryEl) {
-    console.log("Category: ", category.firstElementChild.textContent);
-    console.log("Elements: ", category.lastElementChild.children.length);
+    console.log("Category: ", category.querySelectorAll('h2')[0].textContent);
+    console.log("Elements: ", category.querySelectorAll('ul li').length);
 }
 
+
+
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
